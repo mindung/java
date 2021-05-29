363b6d289d92a9392eb37365acc35907c45c5845
mindung <84787341+mindung@users.noreply.github.com>
2021-05-29 15:24:14 +0900

    5/29 공부 내용 업로드!

--- java_study2/src/java_study2/Main.java
+++ java_study2/src/java_study2/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(11);
+		
+		
+		// 람다 공부!
+		new Thread(() -> System.out.println(111));
 	}
 
 }
