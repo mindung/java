e92d4f2dbe1638d5168c3af22771c2195b1a0e6b
mindung <84787341+mindung@users.noreply.github.com>
2021-06-01 20:00:43 +0900

    Thread 사용해서 폼 움직이기

---
+++ java_swing_ui/src/frame/MoveFrame.java
@@ -0,0 +1,46 @@
+package frame;
+
+import java.sql.Connection;
+import java.util.Random;
+
+public class MoveFrame extends BaseFrame{
+
+	public MoveFrame() {
+		super("움직이는 폼", 400, 600);
+		
+		Thread t = new Thread(() -> {
+			int r = 5000;
+			try {
+				while (r > 0) {
+					Thread.sleep(10);
+					
+					Random random = new Random();
+					int x = getX() + random.nextInt() % 5; // 새위치x // rdIdx(); // 
+					int y = getY() + random.nextInt() % 5; // + random.nextInt() % 5; // 새위치y rdIdx();
+					setLocation(x, y); // 프레임의 위치 이동
+					System.out.println(x + ", " + y);
+					r = r - 100;
+					System.out.println(r);
+				}
+				
+			} catch (Exception e) {
+//				return;
+				e.printStackTrace();
+			}
+		
+//			System.out.println(x);
+//			System.out.println(y);
+//			System.out.println("실행");
+		});
+		
+		t.start();
+	}
+	
+	private int rdIdx() {
+		int randomIdx = (int) (Math.random() * 9) + 1;
+		return randomIdx;
+	}
+	public static void main(String[] args) {
+		new MoveFrame().setVisible(true);
+	}
+}
