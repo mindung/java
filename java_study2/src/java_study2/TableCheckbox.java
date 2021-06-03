ada3705904b67ba2bc13dad723842b4d0714d775
mindung <84787341+mindung@users.noreply.github.com>
2021-06-03 21:50:08 +0900

    Overide Study

---
+++ java_study2/src/java_study2/TableCheckbox.java
@@ -0,0 +1,61 @@
+package java_study2;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableModel;
+
+public class TableCheckbox extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JTable table;
+
+    
+    public TableCheckbox() {
+        Object[] columnNames = {"Type", "Company", "Shares", "Price", "Boolean"};
+        Object[][] data = {
+            {"Buy", "IBM", new Integer(1000), new Double(80.50), false},
+            {"Sell", "MicroSoft", new Integer(2000), new Double(6.25), true},
+            {"Sell", "Apple", new Integer(3000), new Double(7.35), true},
+            {"Buy", "Nortel", new Integer(4000), new Double(20.00), false}
+        };
+        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+        
+        table = new JTable(model) {
+
+            @Override
+            public Class getColumnClass(int column) {
+                switch (column) {
+                    case 0:
+                        return String.class;
+                    case 1:
+                        return String.class;
+                    case 2:
+                        return Integer.class;
+                    case 3:
+                        return Double.class;
+                    default:
+                        return Boolean.class;
+                }
+            }
+        };
+        table.setPreferredScrollableViewportSize(table.getPreferredSize());
+        JScrollPane scrollPane = new JScrollPane(table);
+        getContentPane().add(scrollPane);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+            	TableCheckbox frame = new TableCheckbox();
+                frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setLocation(150, 150);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
